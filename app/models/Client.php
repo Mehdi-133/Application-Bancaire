07eb33f697ce2c3675975f9879265530ccfb8db6
Mehdi-133 <mehdikarbitou@gmail.com>
2025-12-25 16:47:08 +0100

    update client class

--- app/models/Client.php
+++ app/models/Client.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once "BaseModel.php";
+require_once  __DIR__  .  "/../../repositories/client_repos.php";
 
 
 
@@ -18,11 +18,14 @@ class Client extends ClientRepository
     }
 
 
+    public function setId($id){
+        $this->id = $id;
+    }
     public function getId(): ?int
     {
         return $this->id;
     }
-
+    
     public function getName(): string
     {
         return $this->nom;
@@ -36,7 +39,7 @@ class Client extends ClientRepository
     public function setName(string $nom): void
     {
         if (strlen($nom) < 3) {
-            throw new Exception("Name must contain at least 3 characters");
+            echo "Name must contain at least 3 characters";
         }
         $this->nom = $nom;
     }
@@ -44,7 +47,7 @@ class Client extends ClientRepository
     public function setEmail(string $email): void
     {
         if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-            throw new Exception("Invalid email address");
+            echo "Invalid email address";
         }
         $this->email = $email;
     }
--- app/models/Compte.php
+++ app/models/Compte.php
@@ -1,33 +1,35 @@
 <?php
 
-require_once "BaseModel.php";
+require_once  "/../repositories/client_repos.php";
 
-abstract class Compte extends ClientRepository{
+abstract class Compte extends CompteRepository{
 
 
     
 
     protected int $client_id;
     protected string $numero;
-    protected float $solde;
+    protected float $sold;
     protected string $type_compte;
 
 
-    public function __construct(int $client_id, string $numero, string $type_compte, float $solde = 0)
+    public function __construct(int $client_id, string $numero, string $type_compte, float $sold = 0)
     {
         parent::__construct();
         $this->client_id = $client_id;
         $this->numero = $numero;
         $this->type_compte = $type_compte;
-        $this->solde = $solde;
+        $this->sold = $sold;
 
     }
 
     public function getSolde(): float
     {
-        return $this->solde;
+        return $this->sold;
     }
 
     abstract public function deposer(float $montant): void;
     abstract public function retirer(float $montant): void;
 }
+
+
