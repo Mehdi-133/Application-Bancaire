fe45ddfb511c4b1818292536b20e4a5ce90d5492
Mehdi-133 <mehdikarbitou@gmail.com>
2025-12-26 09:40:36 +0100

    BAN-10 Consultation  tous les comptes

--- repositories/compte_repos.php
+++ repositories/compte_repos.php
@@ -6,13 +6,49 @@ require_once __DIR__ . "/../app/config/database.php";
 class CompteRepository
 {
 
-    private $pdo;
+    private PDO $pdo;
 
     public function __construct()
     {
         $db = new db();
         $this->pdo = $db->connect();
     }
+
+    public function createCompte(int $client_id , string $numero , string $type_compte , float $sold = 0) :bool
+    {
+       if ($client_id <= 0 || trim($numero) === "")  {
+        return false;
+       }
+
+       elseif(!in_array($type_compte , ["courant ", "epargne"])){
+
+        return false;
+
+       }
+
+       $sql = "INSERT INTO comptes (client_id , numero , type_compte , sold) VALUES (:clientid , :numero , :type_cpt , :sold ) " ;
+       $stmt = $this->pdo->prepare($sql);
+       return  $stmt ->execute([':clientid'=>$client_id ,':numero'=> $numero ,':type_cpt'=> $type_compte,':sold'=> $sold]);
+
+    }
+
+
+    public function getALLCompte(){
+
+        $stmt = $this->pdo->query("SELECT * FROM comptes");
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        }
+
+    
+    
+        
+
+
+
+
+
+
 
     
 }
